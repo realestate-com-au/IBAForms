b23d390bcac5ec58cfd0bd7efe41796567ad778d
Anthony Mittaz <anthony.mittaz@rea-group.com>
2013-01-25 13:31:51 +1100

    Revert "Decide on navigation toolbar visibility earlier in the process"
    This reverts commit ba1f06c844cfceafe72e0a192cc6a43eab37d8a3.

--- library/src/input/controllers/IBAInputManager.m
+++ library/src/input/controllers/IBAInputManager.m
@@ -245,8 +245,6 @@
         return [inputProviderCoordinator_ setInputView:inputProvider.view];
     }
 
-    [self updateInputNavigationToolbarVisibility];
-
     if (requestor.displayStyle == IBAInputRequestorDisplayStylePopover && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
 
         //prevent the keyboard from appearing
@@ -270,6 +268,8 @@
         if (inputProvider.view != nil) {
             [[requestor responder] setInputView:inputProvider.view];
         }
+
+        [self updateInputNavigationToolbarVisibility];
     }
 
 }
